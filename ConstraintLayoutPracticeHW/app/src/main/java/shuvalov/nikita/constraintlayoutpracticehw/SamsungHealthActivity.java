bc26e889be9286b271116789089fa6df941ecfc2
NikShuvalov <nshuvalov89@gmail.com>
2016-10-30 22:59:06 -0400

    Finished Grubhub constraint layout view

---
+++ ConstraintLayoutPracticeHW/app/src/main/java/shuvalov/nikita/constraintlayoutpracticehw/SamsungHealthActivity.java
@@ -0,0 +1,13 @@
+package shuvalov.nikita.constraintlayoutpracticehw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SamsungHealthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samsung_health);
+    }
+}
