d56976ea24ac873f76916a091d9daa1ee79a0e8a
NikShuvalov <nshuvalov89@gmail.com>
2016-10-31 00:17:43 -0400

    Finished Google Opinion rewards activity, made 3 buttons to switch between activities, fixed up some issues after testing.

--- ConstraintLayoutPracticeHW/app/src/main/java/shuvalov/nikita/constraintlayoutpracticehw/MainActivity.java
+++ ConstraintLayoutPracticeHW/app/src/main/java/shuvalov/nikita/constraintlayoutpracticehw/MainActivity.java
@@ -1,7 +1,9 @@
 package shuvalov.nikita.constraintlayoutpracticehw;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +13,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button grubHubButt, pandoraButt, samsungButt;
+        Button grubHubButt, pandoraButt, googlerewardsButt;
 
+        grubHubButt = (Button)findViewById(R.id.grubhub);
+        pandoraButt =(Button) findViewById(R.id.pandora);
+        googlerewardsButt = (Button)findViewById(R.id.googlerewards);
 
+        grubHubButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GrubHubActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        pandoraButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PandoraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        googlerewardsButt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (MainActivity.this, GoogleOpinionRewardsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
