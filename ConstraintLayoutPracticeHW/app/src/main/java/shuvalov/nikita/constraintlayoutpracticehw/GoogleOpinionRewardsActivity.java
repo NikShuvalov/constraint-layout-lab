d69cd4fad708d639d6edb5f9397d9db1aa385078
NikShuvalov <nshuvalov89@gmail.com>
2016-10-30 23:53:17 -0400

    Finished Google Opinions Reward activity

--- ConstraintLayoutPracticeHW/app/src/main/java/shuvalov/nikita/constraintlayoutpracticehw/GoogleOpinionRewardsActivity.java
+++ ConstraintLayoutPracticeHW/app/src/main/java/shuvalov/nikita/constraintlayoutpracticehw/GoogleOpinionRewardsActivity.java
@@ -3,11 +3,11 @@ package shuvalov.nikita.constraintlayoutpracticehw;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SamsungHealthActivity extends AppCompatActivity {
+public class GoogleOpinionRewardsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_samsung_health);
+        setContentView(R.layout.activity_google_opinion_rewards);
     }
 }
